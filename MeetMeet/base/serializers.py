dcfbea7f8b052f2f7dffa316560fda79a9daea69
soltaniali <soltanibookali1380@gmail.com>
2023-03-23 19:33:06 +0430

    update method serializer & response status added

--- MeetMeet/base/serializers.py
+++ MeetMeet/base/serializers.py
@@ -20,15 +20,39 @@ class UserSerializer(DynamicFieldsModelSerializer):
 class categoriesSerializers(serializers.ModelSerializer):
     class Meta:
         model = models.Category
-        fields = "__all__"   
+        fields = ("name" , "id")   
 class RoomSerializers(serializers.ModelSerializer):
-    categories = categoriesSerializers(many=True , read_only=True)
+    categories = categoriesSerializers(many=True ) 
     class Meta:
         model = models.Room
         fields = ("title"  ,"room_type" ,"link" , "password" , "description"  , "view_count", "start_date" , "end_date" , "maximum_member_count" , "open_status" , "categories" )
-    # def validate(self, data):
-    #     if data.get('end', 0) > data.get('maximum_price', 0):
-    #         error = 'Maximum should be greater than minimum'
-    #         raise serializers.ValidationError(error)
+    def create(self, validated_data):
+        category_data = validated_data.pop('categories')
+        room = models.Room.objects.create(**validated_data)
+        for category in category_data:
+            id_category = models.Category.objects.get(name=category["name"])
+            room.categories.add(id_category)
+        return room    
+    def update(self,instance , validated_data):
+        
+        categories_data = validated_data.pop('categories')
+        categories = instance.categories
+        cate = []
+        for data in categories_data:
+            cate.append(models.Category.objects.get(name=data["name"]))
+        categories.set(cate)
 
-    #     return data
+        instance.title = validated_data.get('title' , instance.title)
+        instance.room_type = validated_data.get("room_type" , instance.room_type)
+        instance.link = validated_data.get('link' , instance.link)
+        instance.password = validated_data.get('password' , instance.password)
+        instance.description = validated_data.get('description' , instance.description)
+        instance.view_count = validated_data.get("view_count" , instance.view_count)
+        instance.start_date = validated_data.get('start_date' , instance.start_date)
+        instance.end_date = validated_data.get('end_date' , instance.end_date)
+        instance.maximum_member_count = validated_data.get('maximum_member_count' , instance.maximum_member_count)
+        instance.open_status = validated_data.get("open_status" , instance.open_status)
+        
+        instance.save()
+        
+        return instance
--- MeetMeet/base/views.py
+++ MeetMeet/base/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import get_object_or_404
+from rest_framework.status import HTTP_406_NOT_ACCEPTABLE , HTTP_201_CREATED , HTTP_200_OK, HTTP_202_ACCEPTED
 from .serializers import RoomSerializers
 from .models import Room , Category , Membership
 from authentication.models import User
@@ -13,7 +14,7 @@ def Home(request):
     return Response({"success" : "base is working"})
 
 class PrivateMeetViewSet(APIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     serializer_class = RoomSerializers
     def get (self, request):
         user = User.objects.get(username = request.user.username)
@@ -23,32 +24,25 @@ class PrivateMeetViewSet(APIView):
             IDs.append(roomId.room_id)
         userRooms = Room.objects.filter(id__in = IDs)
         serializer_all = RoomSerializers(instance=userRooms , many = True)
-        return Response({"success" : f"{serializer_all.data}"} , 202)
+        return Response( serializer_all.data , status=HTTP_200_OK)
 class PublicMeetViewSet(APIView):
-    permission_classes = [IsAuthenticated] # check is authenticated
+    # permission_classes = [IsAuthenticated] # check is authenticated
     serializers = RoomSerializers
     def get (self, request):
         queryset = Room.objects.all()
-        all_serializers = RoomSerializers(instance= queryset , many = True)
-        return Response({"success" : f"{all_serializers.data}"} , 202)
+        all_serializers = RoomSerializers(queryset , many = True)
+        return Response(all_serializers.data , status=HTTP_202_ACCEPTED)
     def post (self, request):
         room_serializers = RoomSerializers(data = request.data)
         if room_serializers.is_valid():
             room = room_serializers.save()
-            for tag_id in request.data.get('categories'):
-                try:
-                    cate = Category.objects.get(id=tag_id)
-                    room.categories.add(cate)
-                except cate.DoesNotExist:
-                    raise cate.NotFound()
-
-            return Response({"successfully added!"} , 202)
+            return Response({"success" : "created!"} , status=HTTP_201_CREATED)
         else :
-            return Response({"Failed to add"})
+            return Response({"fail" : "not valid data"} , status=HTTP_406_NOT_ACCEPTABLE)
 class PublicMeetDeleteUpdate(APIView):
     # if admin 
     serializer_class  = RoomSerializers
-    permission_classes = [IsAdmin , IsAuthenticated] # check is authenticated & check the user sends & add the creator 
+    # permission_classes = [IsAdmin , IsAuthenticated] # check is authenticated & check the user sends & add the creator 
     def put (self, request , room_id):
         room = get_object_or_404(Room , id = room_id)
         self.check_object_permissions(request, room)
@@ -59,10 +53,10 @@ class PublicMeetDeleteUpdate(APIView):
         )
         if car_serializer.is_valid():
             car_serializer.save()
-            return Response({"successfully" : "added!"} , 202)
-        return Response({"message" : car_serializer.errors})
+            return Response({"success" : "added!"} , status=HTTP_202_ACCEPTED)
+        return Response({"fail" : car_serializer.errors})
     def delete (self, request , room_id):
         room = get_object_or_404(Room , id = room_id)
         self.check_object_permissions(request , room)
         room.delete()
-        return Response({"succsess" : "deleted!"} , 204)
+        return Response({"success" : "deleted!"} , status=HTTP_200_OK)
