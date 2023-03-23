9a3456e5886a28807d50df71b5737cee85ab270c
soltaniali <soltanibookali1380@gmail.com>
2023-03-23 23:33:38 +0430

    permissions bug fixed

--- MeetMeet/base/permissions.py
+++ MeetMeet/base/permissions.py
@@ -3,14 +3,15 @@ import sys
 sys.path.append("..") # Adds higher directory to python modules path.
 from .models import Membership
 from authentication.models import User
+from rest_framework.generics import get_object_or_404
 
 class IsAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
         # user = User.objects.get(username = request.user.username)
-        theUser = Membership.objects.filter(room_id = obj.id , member_id = request.user.id )
-        if theUser[0].is_owner == True :
+        theUser = get_object_or_404(Membership , room_id = obj.id , member_id = request.user.id )
+        if theUser.is_owner == True :
             return True
         return False
 
